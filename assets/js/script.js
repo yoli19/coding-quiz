e4d095a2cb9f6a37bd2ed06c276bf2ba1336b299
Yolanda Jacobs <y.jacobs@hotmail.com>
2022-04-11 22:50:16 -0400

    loop questions and check answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,8 +55,14 @@ var startQuizScreenEl = document.querySelector("#start-quiz-screen");
 var quizQuestionScreenEl = document.querySelector("#quiz-question-screen");
 var saveScoreScreenEl = document.querySelector("#save-score-screen");
 var viewScoresScreenEl = document.querySelector("#view-scores-screen");
+var startQuizBtn = document.querySelector("#start-quiz-btn");
+var question = document.querySelector("#question");
+var yourScore = document.querySelector("#your-score");
+var a = document.querySelector("#a");
+var b = document.querySelector("#b");
+var c = document.querySelector("#c");
+var d = document.querySelector("#d");
 
-var startQuizBtn = document.querySelector("#start-quiz-btn")
 
 var timeLeft = document.querySelector("#time-left");
 var timer = document.querySelector("#timer");
@@ -65,7 +71,7 @@ var questionsIndex = 0
 var score = 0
 
 
-//When I click the start button then a timer starts and I am presented with a question.
+//WHEN I click the start button THEN a timer starts and I am presented with a question
 var startQuiz = function() {
     startQuizScreenEl.style.display = "none"
     quizQuestionScreenEl.style.display = "block"
@@ -83,6 +89,61 @@ var startQuiz = function() {
             }
         }
     },1000);
+
+    quizQuestions();
 };
 
+//WHEN I answer a question THEN I am presented with another question
+var quizQuestions = function() {
+    //display each question's text content
+    question.textContent = questions[questionsIndex].question;
+    a.textContent = questions[questionsIndex].answers[0];
+    b.textContent = questions[questionsIndex].answers[1];
+    c.textContent = questions[questionsIndex].answers[2];
+    d.textContent = questions[questionsIndex].answers[3];
+
+
+};
+
+//check answer. if correct add 1 to score, else subtract 10 from time
+var checkAnswer = function(x) {
+    //WHEN I answer a question incorrectly THEN time is subtracted from the clock
+    if (questions[questionsIndex].answers[x] === questions[questionsIndex].correct) {
+        score++
+    } else {
+        time -= 10
+        timer.textContent = time
+    };
+
+    questionsIndex++;
+
+    if (questionsIndex < questions.length) {
+        quizQuestions();
+    } else {
+        quizOver();
+    };
+};
+
+
+
+//WHEN all questions are answered or the timer reaches 0 THEN the game is over
+var quizOver = function() {
+    quizQuestionScreenEl.style.display = "none"
+    saveScoreScreenEl.style.display = "block"
+    yourScore.textContent = score
+};
+
+//WHEN the game is over THEN I can save my initials and score
+
 startQuizBtn.addEventListener("click", startQuiz);
+
+//listen for button click on answers
+a.addEventListener("click", clickA);
+b.addEventListener("click", clickB);
+c.addEventListener("click", clickC);
+d.addEventListener("click", clickD);
+
+function clickA() { checkAnswer(0); };
+function clickB() { checkAnswer(1); };
+function clickC() { checkAnswer(2); };
+function clickD() { checkAnswer(3); };
